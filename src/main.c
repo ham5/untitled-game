ce581b6a998553a72ebabd9447631ea75e30024d
Heitor Machado <ham5@cin.ufpe.br>
2025-08-08 19:54:46 -0300

    feat: implementação inicial do menu principal

--- src/main.c
+++ src/main.c
@@ -4,51 +4,75 @@ This is an example main file for a simple raylib project.
 Use this as a starting point or replace it with your code.
 
 by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
 */
 
 #include "raylib.h"
+#include "resource_dir.h"	// biblioteca auxiliar criada para para garantir que arquivos como imagens, sons e fontes possam ser carregados corretamente, mesmo que o executável esteja em outro diretório
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+typedef enum{
+	INIT,
+	PLAY,
+	CONFIGURATIONS,
+	DEVELOPERS
+}GameScreen;
 
-int main ()
-{
-	// Tell the window to use vsync and work on high DPI displays
+int main (){
+	// informa à janela para usar V-Sync e funcionar em telas com alta densidade de pixels (HiDPI)
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	// define largura e altura da tela
+	const int screenWidth = 1280;
+	const int screenHeight = 720;
+	
+	// inicia a janela e o contexto OpenGL
+	InitWindow(screenWidth, screenHeight, "Untitled Game");
 
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+	// função utilitária de resource_dir.h para encontrar a pasta de recursos e defini-la como diretório de trabalho atual, assim podemos carregar as imagens a partir dela
 	SearchAndSetResourceDir("resources");
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("characters/wabbit_alpha.png");
-	
-	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
-	{
-		// drawing
+	GameScreen currentScreen = INIT;	// programa inicia no menu principal
+	bool exit = false;	// flag para sair
+
+	// executa o loop até o usuário pressionar ESC, clicar no botão de fechar da janela ou clicar na opção SAIR
+	while (!WindowShouldClose() && !exit){
+
+		int MenuFontSize = 30;
+
+		// ------------ desenha a tela ----------------
 		BeginDrawing();
+			ClearBackground(RAYWHITE); // limpa o background
 
-		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
+			switch(currentScreen){
+				case INIT:
+					DrawText("JOGAR", (GetScreenWidth() - MeasureText("JOGAR", MenuFontSize)) / 2, (GetScreenHeight() - MenuFontSize) / 2 + 100, MenuFontSize, LIGHTGRAY);
 
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+					DrawText("CONFIGURAÇÕES", (GetScreenWidth() - MeasureText("CONFIGURAÇÕES", MenuFontSize)) / 2, (GetScreenHeight() - MenuFontSize) / 2 + 150, MenuFontSize, LIGHTGRAY);
 
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
-		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+					DrawText("DESENVOLVEDORES", (GetScreenWidth() - MeasureText("DESENVOLVEDORES", MenuFontSize)) / 2,  (GetScreenHeight() - MenuFontSize) / 2 + 200, MenuFontSize, LIGHTGRAY);
+
+					DrawText("SAIR", (GetScreenWidth() - MeasureText("SAIR", MenuFontSize)) / 2,  (GetScreenHeight() - MenuFontSize) / 2 + 250, MenuFontSize, LIGHTGRAY);
+					break;
+						
+				case PLAY:
+					DrawText("TELA DE JOGO", (GetScreenWidth() - MeasureText("TELA DE JOGO", MenuFontSize)) / 2, (GetScreenHeight() - MenuFontSize) / 2, MenuFontSize, LIGHTGRAY);
+					break;
+					
+				case CONFIGURATIONS:
+					DrawText("CONFIGURAÇÕES", (GetScreenWidth() - MeasureText("CONFIGURAÇÕES", MenuFontSize)) / 2, (GetScreenHeight() - MenuFontSize) / 2, MenuFontSize, LIGHTGRAY);
+					break;
+					
+				case DEVELOPERS:
+					DrawText("DESENVOLVEDORES", (GetScreenWidth() - MeasureText("DESENVOLVEDORES", MenuFontSize)) / 2,  (GetScreenHeight() - MenuFontSize) / 2, MenuFontSize, LIGHTGRAY);
+					break;
+				
+				default:
+					break;
+			}
+
 		EndDrawing();
 	}
-
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
-
-	// destroy the window and cleanup the OpenGL context
+	// destrói a janela e limpa o contexto OpenGL
 	CloseWindow();
+	
 	return 0;
 }
