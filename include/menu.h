16122e0e101ae53c1150cce06c738f406564ba20
Heitor Machado <ham5@cin.ufpe.br>
2025-08-09 19:14:43 -0300

    refactor: modularizar main.c com funções do menu em arquivo separado

---
+++ include/menu.h
@@ -0,0 +1,26 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "raylib.h"
+
+typedef enum{
+	INIT,
+	PLAY,
+	CONFIGURATIONS,
+	DEVELOPERS
+}GameScreen;
+
+typedef struct{
+	GameScreen currentScreen;
+	bool audio;
+	bool exit;
+}GameState;
+
+bool clickedIn(const Rectangle ButtonArea, const Vector2 mousePosition);
+void updateGameState(GameState *const state, const int fontSize);
+void inputAudioConfig(const int fontSize, const bool audio);
+void showInitScreen(const int fontSize);
+void showConfigScreen(const int fontSize, const bool audio);
+void showDevelopScreen(const int fontSize);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -9,20 +9,15 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 #include "raylib.h"
 #include "resource_dir.h"	// biblioteca auxiliar criada para para garantir que arquivos como imagens, sons e fontes possam ser carregados corretamente, mesmo que o executável esteja em outro diretório
 
-typedef enum{
-	INIT,
-	PLAY,
-	CONFIGURATIONS,
-	DEVELOPERS
-}GameScreen;
+#include "menu.h"
 
 int main (){
 	// informa à janela para usar V-Sync e funcionar em telas com alta densidade de pixels (HiDPI)
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-	// define largura e altura da tela (não alterar aqui)
-	const int screenWidth = 1280;
-	const int screenHeight = 720;
+	// define largura e altura da tela
+	const int screenWidth = 900;
+	const int screenHeight = 900;
 	
 	// inicia a janela e o contexto OpenGL
 	InitWindow(screenWidth, screenHeight, "Untitled Game");
@@ -30,43 +25,34 @@ int main (){
 	// função utilitária de resource_dir.h para encontrar a pasta de recursos e defini-la como diretório de trabalho atual, assim podemos carregar as imagens a partir dela
 	SearchAndSetResourceDir("resources");
 
-	GameScreen currentScreen = INIT;	// programa inicia no menu principal
-	bool exit = false;	// flag para sair
+	GameState state = {.currentScreen = INIT,
+					   .audio = true,
+					   .exit = false };
+	int fontSize = 30;
 
 	// executa o loop até o usuário pressionar ESC, clicar no botão de fechar da janela ou clicar na opção SAIR
-	while (!WindowShouldClose() && !exit){
-
-		const int MenuFontSize = 30;
+	while (!WindowShouldClose() && !state.exit){
 
 		// ------------ desenha a tela ----------------
 		BeginDrawing();
 			ClearBackground(RAYWHITE); // limpa o background
 
-			switch(currentScreen){
+			switch(state.currentScreen){
 				case INIT:
-					DrawText("JOGAR", (GetScreenWidth() - MeasureText("JOGAR", MenuFontSize)) / 2, (GetScreenHeight() - MenuFontSize) / 2 + 100, MenuFontSize, LIGHTGRAY);
-
-					DrawText("CONFIGURAÇÕES", (GetScreenWidth() - MeasureText("CONFIGURAÇÕES", MenuFontSize)) / 2, (GetScreenHeight() - MenuFontSize) / 2 + 150, MenuFontSize, LIGHTGRAY);
-
-					DrawText("DESENVOLVEDORES", (GetScreenWidth() - MeasureText("DESENVOLVEDORES", MenuFontSize)) / 2,  (GetScreenHeight() - MenuFontSize) / 2 + 200, MenuFontSize, LIGHTGRAY);
-
-					DrawText("SAIR", (GetScreenWidth() - MeasureText("SAIR", MenuFontSize)) / 2,  (GetScreenHeight() - MenuFontSize) / 2 + 250, MenuFontSize, LIGHTGRAY);
+					showInitScreen(fontSize);
 					break;
 						
 				case PLAY:
-					DrawText("TELA DE JOGO...", (GetScreenWidth() - MeasureText("TELA DE JOGO...", MenuFontSize)) / 2, (GetScreenHeight() - MenuFontSize) / 2, MenuFontSize, LIGHTGRAY);
+					// rodar o jogo aqui
 					break;
 					
-				case CONFIGURATIONS:
-					DrawText("CONFIGURAÇÕES", (GetScreenWidth() - MeasureText("CONFIGURAÇÕES", MenuFontSize)) / 2, (GetScreenHeight() - MenuFontSize) / 2 - 250, MenuFontSize, LIGHTGRAY);
-
-					DrawText("VOLTAR", (GetScreenWidth() - MeasureText("VOLTAR", MenuFontSize)) / 2,  (GetScreenHeight() - MenuFontSize) / 2 + 250, MenuFontSize, LIGHTGRAY);
+				case CONFIGURATIONS:{
+					showConfigScreen(fontSize, state.audio);
 					break;
+				}
 					
 				case DEVELOPERS:
-					DrawText("DESENVOLVEDORES", (GetScreenWidth() - MeasureText("DESENVOLVEDORES", MenuFontSize)) / 2,  (GetScreenHeight() - MenuFontSize) / 2 - 250, MenuFontSize, LIGHTGRAY);
-
-					DrawText("VOLTAR", (GetScreenWidth() - MeasureText("VOLTAR", MenuFontSize)) / 2,  (GetScreenHeight() - MenuFontSize) / 2 + 250, MenuFontSize, LIGHTGRAY);
+					showDevelopScreen(fontSize);
 					break;
 				
 				default:
@@ -74,57 +60,9 @@ int main (){
 			}
 
 		EndDrawing();
-		
-		// ------------ armazena as áreas dos botões ----------------
-		Rectangle playButtonArea = {(GetScreenWidth() - MeasureText("JOGAR", MenuFontSize)) / 2, (GetScreenHeight() - MenuFontSize) / 2 + 100, MeasureText("JOGAR", MenuFontSize), MenuFontSize};
-		
-		Rectangle configButtonArea = {(GetScreenWidth() - MeasureText("CONFIGURAÇÕES", MenuFontSize)) / 2, (GetScreenHeight() - MenuFontSize) / 2 + 150, MeasureText("CONFIGURAÇÕES", MenuFontSize), MenuFontSize};
-		
-		Rectangle devButtonArea = {(GetScreenWidth() - MeasureText("DESENVOLVEDORES", MenuFontSize)) / 2, (GetScreenHeight() - MenuFontSize) / 2 + 200, MeasureText("DESENVOLVEDORES", MenuFontSize), MenuFontSize};
-		
-		Rectangle exitButtonArea = {(GetScreenWidth() - MeasureText("SAIR", MenuFontSize)) / 2, (GetScreenHeight() - MenuFontSize) / 2 + 250, MeasureText("SAIR", MenuFontSize), MenuFontSize};
 
-		Rectangle backButtonArea = {(GetScreenWidth() - MeasureText("VOLTAR", MenuFontSize)) / 2, (GetScreenHeight() - MenuFontSize) / 2 + 250, MeasureText("VOLTAR", MenuFontSize), MenuFontSize};
-		
-		// armazena a posição do mouse
-		Vector2 mousePosition = GetMousePosition();
+		updateGameState(&state, fontSize);
 
-		// ------------ atualiza a tela ----------------
-		switch (currentScreen){	// verifica se um dos botões foi pressionado, de acordo com a tela atual
-			case INIT:
-				if (CheckCollisionPointRec(mousePosition, playButtonArea) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){ // se usuário apertou "JOGAR"
-					currentScreen = PLAY;
-
-				} else if (CheckCollisionPointRec(mousePosition, configButtonArea) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){ // se usuário apertou "CONFIGURAÇÕES"
-					currentScreen = CONFIGURATIONS;
-
-				} else if (CheckCollisionPointRec(mousePosition, devButtonArea) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){ // se usuário apertou "DESENVOLVEDORES"
-					currentScreen = DEVELOPERS;
-
-				} else if (CheckCollisionPointRec(mousePosition, exitButtonArea) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){ // se usuário apertou "SAIR"
-					exit = true;
-				}
-				break;
-
-			case PLAY:
-				// em breve haverá um botão de configurações no canto superior direito da tela
-				break;
-
-			case CONFIGURATIONS:
-				if (CheckCollisionPointRec(mousePosition, backButtonArea) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){ // se usuário apertou "VOLTAR"
-					currentScreen = INIT;
-				}
-				break;
-
-			case DEVELOPERS:
-				if (CheckCollisionPointRec(mousePosition, backButtonArea) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){ // se usuário apertou "VOLTAR"
-					currentScreen = INIT;
-				}
-				break;
-			
-			default:
-				break;
-		}
 	}
 	// destrói a janela e limpa o contexto OpenGL
 	CloseWindow();
